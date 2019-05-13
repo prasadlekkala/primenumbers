e2c5654f137579bc513742f6e44f4552b4b12a87
prasadlekkala <prasadlekkala@users.noreply.github.com>
2019-05-13 14:54:54 +0530

    Update primenumbersprint.php

--- primenumbersprint.php
+++ primenumbersprint.php
@@ -2,7 +2,6 @@
 function primeNumbers($input){
     $count = 0 ;
     $number = 2;
-
     $primes = array();
 	
 	while ($count < $input )
@@ -27,44 +26,40 @@ function primeNumbers($input){
 	}
     return $primes;
 }
-
-
-
     function paint()
     {
         /* Add some padding for readability */
         $padding = strlen(get_value(11 - 1, 11 - 1)) + 4;
-
+        echo "<table>";
         for($i = 0; $i < 11; $i++) {
-
-            paint_border($padding);
-            print("\n");
+            //paint_border($padding);
+            echo "<tr>";
             for($j = 0; $j < 11; $j++) {
                 /* base case: blank will be converted to zero */
                 if ($j == 0 && $i == 0) {
-                    print(sprintf("%' " . $padding . "s", ""));
+                    echo "<td></td>";
                 } else {
-                    print(sprintf("%' " . $padding . "d", get_value($i, $j)));
+                    $p = get_value($i, $j);
+                    echo "<td>$p</td>";
+                    //print(sprintf("%' " . $padding . "d", get_value($i, $j)));
                 }
             }
-            print("\n");
+            echo "</tr>";
         }
-        paint_border($padding);
+        echo "</table>";
+        //paint_border($padding);
     }	
-
     function paint_border($padding)
     {
         for($j = 0; $j < 11; $j++) {
             print(sprintf("%'_" . $padding . "s", "_"));
         }
         print(sprintf("%'_" . $padding . "s", "_"));
-        print("\n");
+        echo '<br>';
     }
-
 function get_value($i, $j)
     {
     $primes = primeNumbers(10);
-
         if ($j < $i) {
             return get_value($j, $i);
         } else {
@@ -75,14 +70,5 @@ function get_value($i, $j)
             }
         }
     }
-function dump_primes()
-    {
-    $primes = primeNumbers(10);
-        foreach($primes as $prime) {
-            print(sprintf("%s\t", $prime));
-        }
-        print("\n");
-    }
-dump_primes();
 paint();
 ?>
